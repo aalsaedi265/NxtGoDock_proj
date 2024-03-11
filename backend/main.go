0a3174a4ba4e62ee735dde308e43361f285f8079
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-03-11 07:48:56 -0400

    users funcitons

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
+
 	// "fmt"
 	"log"
 	"os"
@@ -62,5 +64,35 @@ func enableCORS(next http.Handler) http.Handler{
 		// Pass down the request to the next middleware (or final handler)
 		next.ServeHTTP(w, r)
 	})
+}
 
+func jsonContentTypeMiddleware(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w,r)
+	})
+}
+
+func getUsers(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM users")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		users := []User{} // array of users
+		for rows.Next() {
+			var u User
+			if err := rows.Scan(&u.Id, &u.Name, &u.Email); err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(users)
+	}
 }
